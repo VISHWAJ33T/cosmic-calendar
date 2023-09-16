8145adbb72ca5dada371a9629d21ec0ac4a4b32d
Your NameVishwajeet <vishwajeety14122@gmail.com>
2023-09-16 13:46:21 +0530

    initial commit

---
+++ src/Background.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Background.css";
+const Background = () => {
+  return (
+    <section class="wrapper">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </section>
+  );
+};
+
+export default Background;
