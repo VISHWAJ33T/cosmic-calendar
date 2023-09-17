cd77740b5b27461d0297abdde20032326294da04
Your NameVishwajeet <vishwajeety14122@gmail.com>
2023-09-17 20:26:26 +0530

    fully responsive

--- src/Components/Meteor.js
+++ src/Components/Meteor.js
@@ -4,7 +4,7 @@ const Meteor = () => {
   return (
     <div>
       <h2 className="meteor-heading">Meteor Shower</h2>
-      {/* <div className="upcoming-meteors">
+      <div className="upcoming-meteors">
         <a href="">Button</a>
         <a href="">Button</a>
         <a href="">Button</a>
@@ -15,15 +15,21 @@ const Meteor = () => {
         <a href="">Button</a>
         <a href="">Button</a>
         <a href="">Button</a>
-      </div> */}
+      </div>
       <div className="single-meteor">
         <div className="left">
           <div className="container">
             <div className="box">
-              <span className="title">Meteor Title</span>
+              <span className="title">Quadrantid</span>
               <div>
-                <p>summary</p>
-                <span>Date</span>
+                <p>3–4 January, 2024 (25-hours)</p>
+                <span>
+                  In the right conditions, the Quadrantids are one of the year’s
+                  best meteor showers, as they feature an average of 25 meteors
+                  per hour at their peak. The Quadrantids’ peak is relatively
+                  short, lasting from about midnight to dawn, but the volume of
+                  meteors makes the experience worthwhile.
+                </span>
               </div>
             </div>
           </div>
